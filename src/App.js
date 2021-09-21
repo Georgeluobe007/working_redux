8f3f837938dfd9b94610276f64b32720b5f381bc
Georgeluobe007 <68495623+Georgeluobe007@users.noreply.github.com>
2021-09-21 18:55:11 +0100

    first component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
-
-function App() {
-  return (
-    <div className="Column text-center">
-   
-   <div>
-     <h3>Working Target</h3>
-    </div>
-    <div>
-    <i className="fas fa-home"></i>
-    </div>
-   
-    </div>
-  );
+import Nav from './Components/Nav';
+import PageNotFound from './Components/PageNotFound';
+import ProductList from './Components/ProductList';
+import { Switch,Route } from 'react-router-dom';
+import Carts from './Components/Carts';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav/>
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/carts" component={Carts}/>
+          <Route component={PageNotFound}/>
+          </Switch>
+       
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/Carts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Carts extends Component {
+    render() {
+        return (
+            <div>
+                <h1>I am the Carts</h1>
+            </div>
+        )
+    }
+}
